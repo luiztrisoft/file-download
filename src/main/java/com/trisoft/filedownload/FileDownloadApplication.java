793ea34cec5a32c73f5c28a4b0266b1a0e9a2dde
Luiz Alberto <luiz.trisoft@gmail.com>
2018-04-24 23:40:10 -0300

    First commit

---
+++ src/main/java/com/trisoft/filedownload/FileDownloadApplication.java
@@ -0,0 +1,12 @@
+package com.trisoft.filedownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileDownloadApplication.class, args);
+	}
+}
